1550a594a1438a72feba06288f1f6b38521b3838
basscee1604 <basscee1604@gmail.com>
2026-02-03 14:38:20 +0000

    chore(config): update dev environment with docker support

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,15 +6,14 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.docker
+    pkgs.docker-compose
   ];
 
   # Sets environment variables in the workspace
   env = {};
+  services.docker.enable = true;
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
